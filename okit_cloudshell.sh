ba9665512c2e1518430b230950d9bc24c091fdf7
Sofiane Mahdjoubi <sofiane.mahdjoubi@gmail.com>
2022-10-20 18:35:02 +0200

    improved logic for cloning

--- okit_cloudshell.sh
+++ okit_cloudshell.sh
@@ -1,9 +1,11 @@
-cd ~
-git force-clone https://github.com/oracle/oci-designer-toolkit
-cd oci-designer-toolkit
 export OKIT_DIR=~/okit
 file="$OKIT_DIR/log/okit.log"
 mkdir -p $(dirname $file) && touch "$file"
+
+$TARGET_DIR = "oci-designer-toolkit"
+cd ~
+git -C "$TARGET_DIR" pull || git clone https://github.com/oracle/oci-designer-toolkit "$TARGET_DIR"
+
 pip install python-magic --user
 tenancy_id=$(oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]")
 python oci-designer-toolkit/visualiser/okit_query.py -p $OCI_CLI_PROFILE -r $OCI_CLI_PROFILE -j ~/okit-$OCI_CLI_PROFILE.json -c $tenancy_id -s
