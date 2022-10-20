db4e555b9d75c4828f31ad560eb02587490c08d7
Sofiane Mahdjoubi <sofiane.mahdjoubi@gmail.com>
2022-10-20 18:42:01 +0200

    improved target

--- okit_cloudshell.sh
+++ okit_cloudshell.sh
@@ -8,4 +8,4 @@ git -C "$TARGET_DIR" pull || git clone https://github.com/oracle/oci-designer-to
 
 pip install python-magic --user
 tenancy_id=$(oci iam compartment list --all --compartment-id-in-subtree true --access-level ACCESSIBLE --include-root --raw-output --query "data[?contains(\"id\",'tenancy')].id | [0]")
-python oci-designer-toolkit/visualiser/okit_query.py -p $OCI_CLI_PROFILE -r $OCI_CLI_PROFILE -j ~/okit-$OCI_CLI_PROFILE.json -c $tenancy_id -s
+python oci-designer-toolkit/visualiser/okit_query.py -p $OCI_CLI_PROFILE -r $OCI_CLI_PROFILE -j okit-$OCI_CLI_PROFILE.json -c $tenancy_id -s
