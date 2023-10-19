9406fb9adfa2bfff6ec0092763b57cab2a255476
Sofiane Mahdjoubi <sofiane.mahdjoubi@gmail.com>
2023-10-19 15:29:13 +0200

    try editing run server

--- containers/docker/run-server.sh
+++ containers/docker/run-server.sh
@@ -29,4 +29,4 @@ export PYTHONPATH=:/okit/visualiser:/okit/okitweb:/okit
 python3 --version
 
 #/usr/local/bin/gunicorn --workers=4 --limit-request-line 0 --timeout 120 --bind=0.0.0.0:80 okitweb.wsgi:app
-/usr/local/bin/gunicorn okitweb.wsgi:app --config /okit/config/gunicorn_http.py
+/usr/local/bin/gunicorn okitweb.wsgi:app --config /okit/config/gunicorn_https.py
